6235b13c51e8531d8d4cae86e92dc550ae9427d2
Jesse Perez <jessep1130@gmail.com>
2020-12-15 10:59:04 -0600

    added axios package and npm installed - got on change to work. Will now work on axios call

--- moviesnob/src/App.js
+++ moviesnob/src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
-
+import axios from 'axios';
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
       movieData: [],
-      movieAverage: 0
-		};
-	}
+      movieAverage: 0,
+      searchedItem: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+
+  handleChange(event) {
+    this.setState({searchedItem: event.target.value});
+  }
+
+  handleSubmit(event) {
+    console.log('test')
+    console.log('SEARCHED!', this.state.searchedItem)
+    event.preventDefault();
+  }
 
 
   render() {
     return(
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <header>
+          MovieSnob
         </header>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+          Movie Title:
+          <input type="text" onChange={this.handleChange}/>
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     )}
 }
